2554857d7f29e10c9340d60668601c862eb43863
HarshTondak <harshtondak007@gmail.com>
2023-08-09 11:23:59 +0530

    initial commit

---
+++ controllers/AuthController.js
@@ -0,0 +1,44 @@
+import getPrismaInstance from "../utils/PrismaClient.js";
+
+export const checkUser = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      return res.json({ msg: "Email is required", status: false });
+    }
+
+    const prisma = getPrismaInstance();
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      return res.json({ msg: "User not found", status: false });
+    } else {
+      return res.json({ msg: "User found", status: true, data: user });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const onBoardUser = async (req, res, next) => {
+  try {
+    const { email, name, about, image: profilePicture } = req.body;
+    if (!email || !name || !profilePicture) {
+      return res.send("Email, Name and ProfilePicture are required");
+    }
+
+    const prisma = getPrismaInstance();
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        about,
+        profilePicture,
+      },
+    });
+
+    return res.json({ msg: "Register Succecc", status: true, user });
+  } catch (error) {
+    next(error);
+  }
+};
